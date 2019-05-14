0c9b013cb2cffb3f360fa6ddd5e2aa2e9c04a287
Gabriel <gabrieldmonteiro@live.com>
2019-05-14 16:04:55 -0300

    Currency Converter

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
   <div id="app">
+    <h1>Currency Converter</h1>
+    <div class="line">
     <Converter currA="USD" currB="BRL"></Converter>    
+    <Converter currA="BRL" currB="USD"></Converter>  
+    </div>      
+    <div class="line">
+    <Converter currA="CAD" currB="BRL"></Converter>    
+    <Converter currA="BRL" currB="CAD"></Converter>  
+    </div>      
+    <div class="line">
+    <Converter currA="EUR" currB="BRL"></Converter>    
+    <Converter currA="BRL" currB="EUR"></Converter>  
+    </div>
+    <footer class="footerStyle">
+      <a href="https://github.com/gabrieldmonteiro" target="blank">github.com/gabrieldmonteiro</a>
+    </footer>
   </div>
 </template>
 
@@ -19,6 +34,8 @@ export default {
 
 <style>
 #app {
+  max-width: 900px;
+  margin: auto;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -26,4 +43,17 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.line{
+  margin-top: 40px;
+display: flex;
+justify-content: space-around;
+}
+
+.footerStyle{
+  margin-top: 20px;
+  text-align: right;
+  font-size: 9px;
+}
+
 </style>
--- src/components/Converter.vue
+++ src/components/Converter.vue
@@ -17,22 +17,38 @@ export default {
       currA_value: "",
       currB_value: 0
     };
-  },
-//sVR3vtn6CnXEVPB6DSXS7vebcXavtk  = API KEY
-//EXAMPLE URL : https://www.amdoren.com/api/currency.php?api_key=IBZzdLmM2yCYaXjgTZ6x&from=EUR&to=GBP&amount=50
+  },  
   methods: {
-    converter() {       
-        let url = "https://www.amdoren.com/api/currency.php?api_key=sVR3vtn6CnXEVPB6DSXS7vebcXavtk&from="+this.currA+"&to="+this.currB+"&amount="
-        +this.currA_value;
+    converter() {
+      let from_to = this.currA + "_" + this.currB;
+      let cA = this.currA;
+      let cB = this.currB;
 
-        //fetch url
-        
-        
+      let url =
+        "https://free.currconv.com/api/v7/convert?q=" +
+        from_to +
+        "&compact=ultra&apiKey=ce3469123d58a045204c";
+
+      //fetch url
+      fetch(url)
+        .then(res =>{
+          return res.json();
+        })
+        .then(json => {          
+          let cot = json[from_to];    
+          console.log("Current value from 1 "+cA+" to 1 "+cB+" : " + cot);      
+          this.currB_value = (cot * parseFloat(this.currA_value)).toFixed(2);
+        })
+        .catch(err => console.error(err));
     }
   }
 };
 </script>
 
-
 <style scoped>
+.converter{
+  padding: 20px;
+  max-width: 300px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+}
 </style>
